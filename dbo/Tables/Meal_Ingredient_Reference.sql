3f11663288e6cf9d492abe8aceddef3edcd7085e
Chris Ferguson <chrisj_fergie@msn.com>
2020-01-07 22:53:03 -0500

    Add project files.

---
+++ dbo/Tables/Meal_Ingredient_Reference.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [dbo].[Meal_Ingredient_Reference](
+	[ParentMeal] [int] NOT NULL,
+	[ParentIngredient] [int] NOT NULL,
+	[ReferenceID] [int] IDENTITY(1,1) NOT NULL,
+ CONSTRAINT [PK_Meal_Ingredient_Reference] PRIMARY KEY CLUSTERED 
+(
+	[ReferenceID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Meal_Ingredient_Reference]  WITH CHECK ADD  CONSTRAINT [FK_Meal_Ingredients_Key] FOREIGN KEY([ParentIngredient])
+REFERENCES [dbo].[MealIngredients] ([IngredientID])
+GO
+
+ALTER TABLE [dbo].[Meal_Ingredient_Reference] CHECK CONSTRAINT [FK_Meal_Ingredients_Key]
+GO
+ALTER TABLE [dbo].[Meal_Ingredient_Reference]  WITH CHECK ADD  CONSTRAINT [FK_Meals_Key] FOREIGN KEY([ParentMeal])
+REFERENCES [dbo].[Meals] ([MealID])
+GO
+
+ALTER TABLE [dbo].[Meal_Ingredient_Reference] CHECK CONSTRAINT [FK_Meals_Key]
---
+++ dbo/Tables/Meals.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Meals](
+	[MealID] [int] IDENTITY(1,1) NOT NULL,
+	[MealName] [varchar](50) NOT NULL,
+	[Url] [varchar](200) NULL,
+	[Calories] [int] NOT NULL,
+	[Fat] [int] NOT NULL,
+	[Carbs] [int] NOT NULL,
+	[Protein] [int] NOT NULL,
+	[MealType] [int] NOT NULL,
+	[Leftovers] [bit] NOT NULL,
+	[Servings] [int] NULL,
+ CONSTRAINT [PK_Meals] PRIMARY KEY CLUSTERED 
+(
+	[MealID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
